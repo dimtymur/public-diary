9b4309f1250a236b33474c271670c3beef927eac
dimtymur <63157948+dimtymur@users.noreply.github.com>
2020-06-29 22:03:02 +0300

    code clean up on database functions

--- src/inc/db/db_funcs.php
+++ src/inc/db/db_funcs.php
@@ -1,14 +1,21 @@
 <?php
 require_once $dimport["db/db_conn.php"]["path"];
 
+$sql_query = function($query, $temps=[]) use ($conn) {
+	$sth = $conn->prepare($query);
+	$sth->execute($temps);
+
+	if (preg_match("/^SELECT\b/i", $query)) return $sth->fetchAll();
+};
+
 $record_add = function($table, $attrs) use ($conn) {
 	$cols_str = ""; $cols_temp = "";
 	foreach ($attrs as $col => $val) {
-		$cols_str .= ", $col"; $cols_temp .= ", ?";
+		$cols_str .= ",$col"; $cols_temp .= ",?";
 		$vals[] = $val;
 	}
-	$cols_str   = substr($cols_str, 2, strlen($cols_str));
-	$cols_temp  = substr($cols_temp, 2, strlen($cols_temp));
+	$cols_str   = trim($cols_str, ",");
+	$cols_temp  = trim($cols_temp, ",");
 
 	$sql = "INSERT INTO $table ($cols_str) VALUES ($cols_temp);";
 	$sth = $conn->prepare($sql);
@@ -24,10 +31,10 @@ $records_delete = function($table, $id_col, $id_val) use ($conn) {
 $records_edit = function($table, $id_col, $id_val, $attrs) use ($conn) {
 	$cols_temp = "";
 	foreach ($attrs as $col => $val) {
-		$cols_temp .= ", $col = ?"; $vals[] = $val;
+		$cols_temp .= ",$col=?"; $vals[] = $val;
 	}
-	$vals[] 		= $id_val;
-	$cols_temp  = substr($cols_temp, 2, strlen($cols_temp));
+	$vals[]     = $id_val;
+	$cols_temp  = trim($cols_temp, ",");
 
 	$sql = "UPDATE $table SET $cols_temp WHERE $id_col = ?;";
 	$sth = $conn->prepare($sql);
@@ -39,12 +46,4 @@ $records_get = function($table, $id_col, $id_val) use ($conn) {
 	$sth = $conn->prepare($sql);
 	$sth->execute([$id_val]);
 	return $sth->fetchAll();
-};
-
-$sql_query = function($query, $temps=[]) use ($conn) {
-	$sth = $conn->prepare($query);
-	$sth->execute($temps);
-
-	if (explode(" ", $query)[0] == "SELECT")
-		return $sth->fetchAll();
 };
