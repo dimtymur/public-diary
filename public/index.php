29723e436835a7a95e19c07bbff42499818777dc
dimtymur <63157948+dimtymur@users.noreply.github.com>
2020-11-03 14:15:32 +0200

    changed the appearance of the website and css code clean up

--- public/index.php
+++ public/index.php
@@ -5,6 +5,6 @@ require_once dirname($_SERVER["DOCUMENT_ROOT"]).DIRECTORY_SEPARATOR."autoloader.
 require_once $dimport["inc/gen_funcs.php"]["path"];
 // require_once $dimport["debug/error_show.php"]["path"];
 
-$page = get("page", "home/home_page.php");
+$page = get("page", "home/home_page.phtml");
 if (file_exists($dimport[$page]["path"])) require_once $dimport[$page]["path"];
 else $get_error_page("404 Not Found", "/img/error/404.gif");
