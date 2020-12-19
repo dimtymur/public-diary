064535e4c13f92186a4278e2a39b7b57a893e291
dimtymur <63157948+dimtymur@users.noreply.github.com>
2020-12-20 01:14:59 +0200

    added favicon.ico and fixed some account deletion and database bugs

--- src/app/user/delete_user_request.php
+++ src/app/user/delete_user_request.php
@@ -9,10 +9,6 @@ if (!csrf_check($csrf_key))
 
 require_once $dimport["user/user_funcs.php"]["path"];
 
-if (!$password_validate($_POST["password"], $_POST["password-confirm"]) &&
-    !$password_validate($_POST["password"], $user["password"], false))
-    redirect($dimport["home/settings_page.phtml"]["redirect"]."&error=invalid-password");
-
 $user = $records_get("pd_user", "user_id", $_SESSION["u_id"]);
 if (empty($user))
     redirect($dimport["home/settings_page.phtml"]["redirect"]."&error=invalid-user");
@@ -20,6 +16,10 @@ $user = $user[0];
 
 if ($email_verify_get($user["user_id"]))
     redirect($dimport["home/settings_page.phtml"]["redirect"]."&error=unverified-user");
+
+if (!$password_validate($_POST["password"], $_POST["password-confirm"]) &&
+    !$password_validate($_POST["password"], $user["password"], false))
+    redirect($dimport["home/settings_page.phtml"]["redirect"]."&error=invalid-password");
 
 $email_verify_send(
     $user,
